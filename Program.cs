2da9cbbb20243c4cf9aeccd5823232dc52badba1
Jimena Contreras <jimenacontreras2411@gmail.com>
2026-08-17 11:12:06 -0600

    Ejemplo 2 : secuencias de escape

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+Console.Write("Hello,");
+Console.Write(" ");
+Console.Write("Mena!");
+
+Console.Write("\n\n\n");
+//Generar un programa que muestre atributos en forma de tabla
+Console.WriteLine("Estadisticas en forma de tabla");
+Console.WriteLine("/ Greg  \"El mago\" /");
+Console.WriteLine("\\------------------\\");
+Console.WriteLine("/ Hp\tt100       /");
+Console.WriteLine("/    MP\tt50        \\");
+
+//Ejercicio 2: modificar tabla
+
+
+
+
+
+
+
